33b77037e55264b044bdbff85b15a4d9e97dc1ec
XuuXiao <ammaroaladel@Gmail.com>
2024-04-04 19:55:08 +0100

    making camera class with basics

--- Plugin/src/Camera.cs
+++ Plugin/src/Camera.cs
@@ -1,4 +1,23 @@
+using UnityEngine;
+
 namespace ViralCompany;
-public class Camera {
-    
+public class Camera : GrabbableObject {
+    [SerializeField] public float maxLoudness;
+    [SerializeField] public float minLoudness;
+    [SerializeField] public float minPitch;
+    [SerializeField] public float maxPitch;
+    private System.Random noisemakerRandom;
+    public Animator triggerAnimator;
+    void LogIfDebugBuild(string text) {
+      #if DEBUG
+      Plugin.Logger.LogInfo(text);
+      #endif
+    }
+    public override void Start() {
+        base.Start();
+    }
+    public override void ItemActivate(bool used, bool buttonDown = true) {
+        // logic
+        triggerAnimator?.SetTrigger("playAnim");
+    }
 }
