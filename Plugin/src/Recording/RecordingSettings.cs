3843d62a88693918dc5a518e3e90f6407588cd53
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2024-07-03 16:43:47 +1000

    camera tweaks to make it look more accurate
    still have no clue how the shader is applied but oh well

--- Plugin/src/Recording/RecordingSettings.cs
+++ Plugin/src/Recording/RecordingSettings.cs
@@ -6,6 +6,6 @@ namespace ViralCompany.src.Recording;
 internal static class RecordingSettings {
     // Recording Settings
     internal const int FRAMERATE = 24;
-    internal const int RESOLUTION = 350;
+    internal const int RESOLUTION = 200;
     internal const int BITRATE = 528;
 }
