41e45eb5e4ab410050a8f28bacaa2158f646d979
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2024-07-03 17:04:01 +1000

    discord doesn't like it being that small

--- Plugin/src/Recording/RecordingSettings.cs
+++ Plugin/src/Recording/RecordingSettings.cs
@@ -6,6 +6,6 @@ namespace ViralCompany.src.Recording;
 internal static class RecordingSettings {
     // Recording Settings
     internal const int FRAMERATE = 24;
-    internal const int RESOLUTION = 240;
+    internal const int RESOLUTION = 360;
     internal const int BITRATE = 528;
 }
